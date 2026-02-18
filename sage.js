d84745ee1e0234017b329ed5ac08031f13c41e32
Elucian Moise <elucian.moise@gmail.com>
2026-02-18 14:11:36 -0500

    add border for hr

--- sage.js
+++ sage.js
@@ -37,9 +37,7 @@ function initDynamicHeader() {
         
         <div class="row g-0 mt-2">
             <div class="col ps-0">
-                <hr class="mt-3 mb-0 top-hr">
                 <nav class="breadcrumb-nav">${generateBreadcrumbs()}</nav>
-                <hr class="mt-3 mb-0 top-hr">
             </div>
         </div>`;
 
